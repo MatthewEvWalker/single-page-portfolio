59d17b5c2efb4875b7157b18d7e1c6741fcb581a
Matthew Walker <matthewevwalker@gmail.com>
2023-08-14 21:40:47 -0500

    changed the colors around and the font text. The logo now moves slightly when hovered

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import Drake from "../assets/images/Drake.webp"
+import drake from "../assets/images/Drake.webp"
+import paint from "../assets/images/paint.png"
 import "./Hero.scss";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLightbulb } from "@fortawesome/free-solid-svg-icons";
@@ -21,14 +22,14 @@ const handleSvgClick = () => {
     <section className="hero">
       <div className="splash-container">
         <div className="splash-img">
-          <h1>This is the splash art</h1>
+          <h1>Hi, my name is Matthew</h1>
         </div>
       </div>
       <div className="face-container">
       <FontAwesomeIcon className={`fa ${isBigger ? 'bigger': ''} `} icon={faLightbulb} onClick={handleSvgClick}/>
       {/* <img className="fa" src={unsolid}/> */}
         <div className="face-img">
-          <img src={Drake}/>
+          <img src={drake}/>
         </div>
       </div>
     </section>

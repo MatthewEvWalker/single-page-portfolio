10c98ddf325d94d75327c98d185b16017482f530
Matthew Walker <matthewevwalker@gmail.com>
2023-08-15 00:37:46 -0500

    added media queries that stacks the containers and changed the fonts.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,35 +1,42 @@
 import React, { useState } from "react";
-import drake from "../assets/images/Drake.webp"
-import paint from "../assets/images/paint.png"
+import drake from "../assets/images/Drake.webp";
+import paint from "../assets/images/paint.png";
 import "./Hero.scss";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLightbulb } from "@fortawesome/free-solid-svg-icons";
 import unsolid from "../assets/svgs/unsolid.svg";
 
 function Hero() {
+  const [isBigger, setIsBigger] = useState(false);
 
-const[isBigger, setIsBigger] = useState(false)
+  const handleSvgClick = () => {
+    setIsBigger(true);
 
-const handleSvgClick = () => {
-  setIsBigger(true);
-
-  setTimeout(() => {
-    setIsBigger(false)
-  }, 300)
-}
+    setTimeout(() => {
+      setIsBigger(false);
+    }, 300);
+  };
 
   return (
     <section className="hero">
       <div className="splash-container">
         <div className="splash-img">
-          <h1>Hi, my name is Matthew</h1>
+          <h1>
+            Hi, my <br />
+            name is <b>Matthew</b>
+          </h1>
+          <h2>Testing</h2>
         </div>
       </div>
       <div className="face-container">
-      <FontAwesomeIcon className={`fa ${isBigger ? 'bigger': ''} `} icon={faLightbulb} onClick={handleSvgClick}/>
-      {/* <img className="fa" src={unsolid}/> */}
+        <FontAwesomeIcon
+          className={`fa ${isBigger ? "bigger" : ""} `}
+          icon={faLightbulb}
+          onClick={handleSvgClick}
+        />
+        {/* <img className="fa" src={unsolid}/> */}
         <div className="face-img">
-          <img src={drake}/>
+          <img src={drake} />
         </div>
       </div>
     </section>
